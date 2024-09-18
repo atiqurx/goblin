379e1fe2ad0d7ff364ccc54e8cfae811fe83f0cc
Atiqur Rahman <atiqur.main@gmail.com>
2024-09-17 19:40:08 -0500

    update table and add favicons

--- Models/Category.cs
+++ Models/Category.cs
@@ -16,5 +16,8 @@ namespace goblin.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon { get { return this.Icon + "  " + this.Title; } }
     }
 }
